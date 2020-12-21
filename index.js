905d08b34835a5f8c40800de92e4051e0828bfaf
roman <shmoop207@gmail.com>
2020-12-21 15:37:32 +0200

    added more tests

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.context = exports.Context = exports.namespace = void 0;
 const context_1 = require("./lib/context");
-exports.Context = context_1.Context;
+Object.defineProperty(exports, "Context", { enumerable: true, get: function () { return context_1.Context; } });
 const namespace_1 = require("./lib/namespace");
 let namespace = new namespace_1.Namespace();
 exports.namespace = namespace;
